122a98f5e279c9123ddf8f43abe135fca4dd3e64
Alliyya_Mo <alliyya@mail.uoguelph.ca>
2017-01-23 00:28:52 -0500

    initalize repo with current dot files

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -sv "dot-files/.bash_aliases" ~
+ln -sv "dot-files/.bash_logout" ~
+ln -sv "dot-files/.bashrc" ~
+ln -sv "dot-files/.gitconfig" ~
+ln -sv "dot-files/.nanorc" ~
+ln -sv "dot-files/.profile" ~
+
